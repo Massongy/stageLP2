1856169cc25a623167c99ffa86c8509c374e481e
Ibra-Alpha Ba <ibra-alpha.ba@options.net>
2025-07-11 15:39:28 +0200

    Edit permissions

--- backend/apps/users/utils.py
+++ backend/apps/users/utils.py
@@ -2,7 +2,7 @@ group_permission_map = {
     1: 'can_create_acceor_admin',
     2: 'can_create_options_admin',
     3: 'can_create_acceor_user',
-  #  2: 'can_create_options_user',
+    4: 'can_create_options_user',
 }
 
 can_delete_user_from_group_map = {
